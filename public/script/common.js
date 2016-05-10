5ecf503709dfd73ff481b789722d2bab34b8ebee
alonehover <alonehover@qq.com>
2016-05-10 17:01:05 +0800

    add localStorage test

--- public/script/common.js
+++ public/script/common.js
@@ -1,6 +1,7 @@
-$(function(){
-    $(".card-list").click(function(){
-        var id = $(this).data("item-id");
-        location.href = "/post/" + id;
-    });
+$(function() {
+	$(".card-list").click(function() {
+		var id = $(this).data("item-id");
+		location.href = "/post/" + id;
+		window.localStorage.setItem("name", id)
+	});
 });
